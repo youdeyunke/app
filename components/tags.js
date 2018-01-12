d2ad2c3114882041544c157feaea50295d15b91f
TY@MacBook Pro <tianyu0915@gmail.com>
2018-01-12 14:32:27 +0800

    update

---
+++ components/tags.js
@@ -0,0 +1,24 @@
+// components/tags.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    items: { type: Array, value: [] },
+    right: {type: Boolean, value: false}
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
