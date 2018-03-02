d34f75ba8c7a1f8bc5b4e5fde1042328953a02aa
TY@MacBook Pro <tianyu0915@gmail.com>
2018-03-02 13:28:44 +0800

    update

---
+++ pages/about/index.js
@@ -0,0 +1,78 @@
+// pages/about/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    debugClickCounter: 0,
+  },
+
+  debugHandle :function(e){
+    this.setData({ debugClickCounter: this.data.debugClickCounter + 1})
+    if (this.data.debugClickCounter % 10 == 0){
+      wx.navigateTo({
+        url: '/pages/home/debug',
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/tiao/index.js
@@ -0,0 +1,78 @@
+// pages/tiao/index.js
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    posts: []
+  },
+
+  onLoad: function (options) {
+    var _this = this
+    var d = {}
+    app.loadPosts({ cate: 'item' }, function (resp) {
+      _this.setData({ posts: resp.data })
+      var baseIndex = _this.data.posts.length
+      var dict = {}
+      resp.data.forEach(function(item , i ){
+        var index = baseIndex + i
+        var k = "posts[" + index + "]"
+        console.log('item', item)
+        dict[k] = item
+      })
+      _this.setData(dict)
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
