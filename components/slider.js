82d757a590a4ef269013e7f104e41766870edc35
TY@MacBook Pro <tianyu0915@gmail.com>
2018-04-08 17:12:18 +0800

    update

--- components/slider.js
+++ components/slider.js
@@ -12,11 +12,12 @@ Component({
    */
   data: {
     items: [
-      { src: '/assets/images/b1.jpg', opentype: 'navigateTo', url: '/pages/huodong/index' },   
+      {
+        src: 'http://jiayaosu-10013564.image.myqcloud.com/8d0a4fc3-dd7d-4c2b-88d1-b12f38e96053', opentype: 'navigateTo', url: '/pages/huodong/index' },   
 
-      {src: '/assets/images/b4.jpg', opentype: 'navigateTo',url: '/pages/post/post?id=1'},
+      { src: 'http://jiayaosu-10013564.image.myqcloud.com/01871a47-06ff-4fc8-81de-ba4dcd9505df', opentype: 'navigateTo',url: '/pages/post/post?id=1'},
       
-      {src: '/assets/images/b3.jpg', opentype: 'navigateTo',url: '/pages/posts/index'},
+      { src: 'http://jiayaosu-10013564.image.myqcloud.com/9f3ac103-ab48-4123-933c-b5ce2eaeac70', opentype: 'navigateTo',url: '/pages/posts/index'},
     ]
   },
 
