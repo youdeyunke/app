38f1a8e6d12f6ed463e11d83d25cb5515963ee2b
TY@MacBook Pro <tianyu0915@gmail.com>
2018-05-15 10:30:35 +0800

    新增我的咨询页面

---
+++ pages/myself/qa.js
@@ -0,0 +1,80 @@
+// pages/myself/qa.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.loadData()
+  },
+
+  loadData: function(){
+    var _this = this
+    app.request({
+      url: '/api/v1/myself/questions',
+      success: function(resp){
+        _this.setData({
+          replied: resp.data.data.replied,
+          unreplied: resp.data.data.unreplied
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
