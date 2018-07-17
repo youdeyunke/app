56a20fda5d00135ad9cb0c144071f8ce79e14807
TY@MacBook Pro <tianyu0915@gmail.com>
2018-07-17 18:57:49 +0800

    update

---
+++ components/qas.js
@@ -0,0 +1,23 @@
+// components/qas.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    items: {type: Array, value: [1,1,1,1,1]}
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
