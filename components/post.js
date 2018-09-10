6f67787acfe90761286f462119dd4ad1a6136308
TY@MacBook Pro <tianyu0915@gmail.com>
2018-09-10 16:45:00 +0800

    新增相关推荐

--- components/post.js
+++ components/post.js
@@ -4,9 +4,19 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    border: {
+      type: Boolean, value: true,
+    },
+    shadow: {
+      type: Boolean, value: true,
+    },
     item: {
       type: Object, value: {},
     }
+  },
+
+  ready: function(){
+    console.log('border,', this.data.border, 'shado', this.data.shadow, 'item', this.data.item)
   },
 
   /**
