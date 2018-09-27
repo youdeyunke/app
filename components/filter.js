353e5719f896ad05dc00e38ffe4ba9f91e5f7142
TY@MacBook Pro <tianyu0915@gmail.com>
2018-09-27 17:24:41 +0800

    增加搜索结果过滤器组件

---
+++ components/filter.js
@@ -0,0 +1,28 @@
+// components/filter.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    keys: [
+      ['区域', 'distirct'],
+      ['总价', 'total_price'],
+      ['户型', 'type'],
+    ],
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
