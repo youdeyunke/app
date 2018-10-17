73106f3c98c5b33b5c4367efad13bffb4c15515e
TY@MacBook Pro <tianyu0915@gmail.com>
2018-10-17 18:28:00 +0800

    修改了房源表单的验证方法

--- pages/post/form.js
+++ pages/post/form.js
@@ -7,12 +7,14 @@ Page({
    * 页面的初始数据
    */
   data: {
+    error: {},
     step: 1,
     cities: [],
     districts: [],
     post: {
       city: {},
       district: {},
+      images: [],
     },
 
   },
@@ -20,7 +22,10 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (q) {
+    this.updatePostField('group', q.group || 'rental' )
+    this.updatePostField('rent_type', q.rent_type || 0 )
+    
   },
 
   /**
@@ -44,8 +49,138 @@ Page({
 
   },
 
+
   submit:function(e){
-    console.log('submit e', e)
+    var fdata = e.detail.value
+    var keys = Object.keys(fdata)
+    var post = this.data.post
+    var _this = this
+    // 将form中的字段合并到post
+    keys.forEach(function(key, i){
+      post[key] = fdata[key]
+      if(i == keys.length - 1){
+        _this.setData({post: post})
+      }
+    })
+
+    var name = e.detail.target.dataset.name
+    switch(name){
+      case 'step1':
+        _this.validateStep1(function(){
+          _this.nextStep()
+        })
+        break
+      case 'step2':
+        _this.validateStep2(function(){
+          _this.doSubmit()
+        })
+        break
+    }
+  },
+
+  showError: function(key, msg, alert){
+    console.log('show error ', key, msg)
+    var error = this.data.error
+    error[key] = true
+    this.setData({error: error})
+    if(alert){
+      wx.showToast({title: msg, icon: 'none'})
+    }
+  },
+
+
+
+  validateStep1: function(cb){
+    this.setData({error: {}})
+    var post = this.data.post
+    var _this = this
+    var isok= true
+    
+    if(!post.district_id  || !post.city_id){
+      _this.showError('district_id', '城市、行政区不能为空')
+      isok = false
+    }
+
+    if(!post.area_name){
+      _this.showError('area_name', '请填写小区名称')
+      isok = false
+    }
+
+    if(!post.area_year){
+      _this.showError('area_year', '请填写小区年代')
+      isok = false
+    }
+
+    if(!post.street){
+      _this.showError('street', '请填写详细街道信息')
+      isok = false
+    }
+
+    if(post.group == 'rental' && !post.area){
+      _this.showError('area', '请填写房间面积')
+      isok = false
+    }
+
+    if(post.group == 'old' && !post.construction_area){
+      _this.showError('construction_area', '请填写建筑面积')
+      isok = false
+    }
+
+
+    if(post.group == 'rental' && !post.rent_price){
+      _this.showError('rent_price', '请填写租金')
+      isok = false
+    }
+
+    if(post.group == 'old' && !post.total_price){
+      _this.showError('total_price', '请填写详售价')
+      isok = false
+    }
+
+    if(!post.s && !post.t && !post.w){
+      _this.showError('type', '请填写户型信息')
+      isok = false
+    }
+
+    if(!post.position){
+      _this.showError('type', '请完善户型信息')
+      isok = false
+    }
+
+
+    if(!post.fitment_id){
+      _this.showError('fitment_id', '请填写装修信息')
+      isok = false
+    }
+
+    if(!post.current_floor || !post.total_floor){
+      _this.showError('floor', '请填写楼层信息')
+      isok = false
+    }
+
+
+    if(post.group == 'rental' && !post.payment_cycle){
+      _this.showError('payment_cycle', '请填写租金支付方式')
+      isok = false
+    }
+
+
+    if(post.images.length < 3 || post.images.length > 10){
+      _this.showError('images', '请上传3~10张房源照片', true)
+      isok = false
+    }
+
+    console.log('数据验证成功：', post)
+    if(isok && typeof cb == 'function'){
+      return cb(post)
+    }
+  },
+
+  validateStep2: function(){
+  },
+
+  doSubmit: function(){
+    // update or create post 
   },
 
   nextStep: function(e){
@@ -55,9 +190,13 @@ Page({
   previousStep: function(e){
     this.setData({step: 1})
   },
+
+  clearError: function(){
+    this.setData({error: {}})
+  },
   
-  inputChanged: function(e){
-    console.log('input change', e)
+  inputChange: function(e){
+    this.clearError()
   },
 
   showTypePicker: function(){
@@ -73,15 +212,18 @@ Page({
   },
 
   paymentChanged: function(e){
+    this.clearError()
     this.updatePostField('payment_cycle', e.detail.payment_cycle)
   },
 
   fitmentChanged: function(e){
+    this.clearError()
     this.updatePostField('fitment', e.detail.fitment)
     this.updatePostField('fitment_id', e.detail.fitment.id)
   },
 
   typeChanged: function(e){
+    this.clearError()
     console.log('aaa')
     var post = this.data.post
     post.s = e.detail.s
@@ -92,6 +234,7 @@ Page({
   },
 
   floorChanged: function(e){
+    this.clearError()
     this.updatePostField('current_floor', e.detail.current_floor)
     this.updatePostField('total_floor', e.detail.total_floor)
   },
@@ -105,13 +248,15 @@ Page({
   },
 
   cityChanged: function(e){
-    var post = this.data.post
-    post.city = e.detail.city
-    post.district = e.detail.district
-    this.setData({post : post })
+    this.clearError()
+    this.updatePostField('city', e.detail.city)
+    this.updatePostField('city_id', e.detail.city.id)
+    this.updatePostField('district_id', e.detail.district.id)
+    this.updatePostField('district', e.detail.district)
   },
 
   imagesChanged: function(e){
+    this.clearError()
     console.log('images change', e)
     var keys = Object.keys(e.detail)
     if(keys.includes('images')){
