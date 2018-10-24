d64767d5b4029c904fa8766fca01d20fb570229c
TY@MacBook Pro <tianyu0915@gmail.com>
2018-10-24 10:28:52 +0800

    新增了broker组件

---
+++ components/broker.js
@@ -0,0 +1,58 @@
+// components/broker.js
+const app = getApp()
+var auth = require('../utils/auth.js');
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    broker: {type: Object, value:null},
+    pid: {type: Number, value: null},
+    booking: {type: Number, value: 0},
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    bookingHandle: function () {
+      var _this = this
+      auth.ensureMobile(function (userInfo) {
+        _this._bookingHandle()
+      })
+    },
+
+    _bookingHandle: function () {
+      var pid = this.data.pid
+      var _this = this
+      if (_this.data.booking == 1) {
+        return false
+      }
+      app.request({
+        url: '/api/v1/users/mark_book',
+        method: 'POST',
+        data: { post_id: pid },
+        success: function (resp) {
+          console.log('resp', resp)
+          wx.showModal({
+            title: '预约成功！',
+            content: '经济人稍后会来电与您确认具体看房时间，请留意',
+          })
+          _this.setData({booking: 1})
+        }
+      })
+    },  
+  }
+})
