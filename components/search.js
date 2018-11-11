28933f1eefaaee9cf82a0db6b764dbf68f882cd9
TY@MacBook Pro <tianyu0915@gmail.com>
2018-11-11 10:47:59 +0800

    增加了搜索框

---
+++ components/search.js
@@ -0,0 +1,103 @@
+// components/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    value: {type: String, value: ''},
+    initmode: {type: Number, value: null},
+    fake: {type: Number, value: 0},
+  },
+
+
+  attached: function(){
+    var history = wx.getStorageSync('keywordsHistory') || []
+    var historyTitle = '搜索记录'
+    if (history.length == 0) {
+      historyTitle = '大家都在搜'
+      history = ['世纪公园', '三林']
+    }
+    this.setData({history: history, historyTitle: historyTitle })
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    placeholder: '你想住在哪？',
+    text: '',
+    mode: 1, // 1: 输入模式， 2： 搜索结果模式
+  },
+
+
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    confirm: function(e){
+      console.log('cofirm', e, this.data.text)
+      var text = e.detail.value
+      if(text){
+        this.setData({text: text})
+        this.submit(e)
+      }
+    },
+
+    submit: function(){
+      if(this.data.fake){
+        return false
+      }
+      var text = this.data.text
+      if(text.length == 0){
+        return false
+      }
+      this.triggerEvent('submit', {text: text}, {})
+      this.logHistory(text) 
+    },
+
+    keywordClick: function(e){
+      var text = e.currentTarget.dataset.text
+      this.setData({text: text, value:text})
+    },
+
+    logHistory: function(text){
+      var history = wx.getStorageSync('keywordsHistory') || []
+      history = [text].concat(history)
+      history = [...new Set(history)]
+      wx.setStorageSync('keywordsHistory', history.slice(0, 5))     
+    },
+
+    bindTextInput: function (e) {
+      if(this.data.fake){
+        return false
+      }
+      this.setData({
+        text: e.detail.value,
+        mode: 1,
+        initmode: null,
+      })
+    },    
+
+    searchTap: function(e){
+      if(!this.data.fake){
+        return false
+      }
+      // 跳转到搜索页面
+      wx.navigateTo({
+        url: '/pages/search/index'
+      })
+    },
+
+    clear: function(e){
+      if(this.data.fake){
+        return false
+      }
+      this.setData({text: '', mode: 1, initmode: null})
+      this.triggerEvent('clear', {}, {})
+    },
+
+    inputHandle: function(e){
+    }
+  }
+})
