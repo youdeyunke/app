1d55b7e7192a03fd8f0fd2ea344c8ab4ac7bb7d9
TY@MacBook Pro <tianyu0915@gmail.com>
2018-11-15 15:40:29 +0800

    新增home-icons组件

---
+++ components/home-icons.js
@@ -0,0 +1,90 @@
+// components/home-icons.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    publishSheetShow: false,
+    publishActions: [
+      {name: '我要找房', group: 'need-room'},
+      {name: '我要找室友', group: 'need-roommate'},
+      {name: '我要转租', group: 'rental', is_sublet: true},
+
+      {name: '发布整租房源', group: 'rental', rent_type: 'zhengzu'},
+      {name: '发布合租房源', group: 'rental', rent_type: 'hezu' },
+      {name: '发布二手房房源', group: 'old'  },
+
+    ],
+
+    colors: [
+      '4BC587',
+      'F7B264',
+      'F5D04F',
+      'F17276',
+      '67B1FD',
+      'F7B263',
+      '62D182',
+      '5AC0E5',
+      '5DD2B9',
+      '9291DF',
+    ],
+
+    icons: [
+      {name: '新房', url: '/pages/post/index?group=xinfang', opentype:"navigateTo", id: 'new', bg: '#fdaa3d'},
+      { name: '二手', url: '/pages/post/index?group=ershoufang', opentype:"navigateTo", id: 'old', bg: '#53d8e3'},
+
+      { name: '整租', url: '/pages/post/index?group=zufang&rent_type=zhengzu', opentype:"navigateTo", id: 'rent_house', bg: '#65b455'},
+      { name: '合租', url: '/pages/post/index?group=zufang&rent_type=hezu', opentype:"navigateTo", id: 'rent_room', bg: '#d578e3'},
+
+      { name: '找室友', url: '/pages/need/roommate', opentype: "navigateTo", id: 'shiyou', bg: '#4c9ef0' },
+
+      { name: '找房', url: '/pages/need/room', opentype: "navigateTo", id: 'zhao', bg: '#f67350' }      ,
+
+      { name: '求购', url: '/pages/need/room-form?cat=buy', opentype: "navigateTo", id: 'qiugou'  },
+      { name: '求租', url: '/pages/need/room-form?cat=rent', opentype: "navigateTo", id: 'qiugou'  },
+
+      { name: '发布', url: '', opentype: "", id: 'publish', tapHandle: 'publishHandle'},
+
+    ]
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    publishClose: function(e){
+      this.setData({
+        publishSheetShow: false
+      })
+    },
+
+    publishClick: function(e){
+      var group = e.detail.group
+      var url =  '/pages/post/form?group=' 
+      url += e.detail.group 
+      url +=  '&rent_type=' 
+      url += e.detail.rent_type || 'zhengzu'
+      url += '&is_sublet='
+      url += e.detail.is_sublet || 'false'
+      wx.navigateTo({
+        url: url,
+      })
+    },
+
+    publishHandle: function(e){
+      // 点击发布按钮，弹出选择框
+      this.setData({
+        publishSheetShow: true,
+      })
+    },
+
+  }
+})
