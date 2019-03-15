adc79526adb7450643b87576bd042f7f39c3072d
TY@MacBook Pro <tianyu0915@gmail.com>
2019-03-16 07:46:43 +0800

    update compoments

---
+++ components/sub-district/items.js
@@ -0,0 +1,106 @@
+// components/sub-district/items.js
+const app = getApp()
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    filter: {
+      type: Object, value: {}, observer: "filterChange"
+    },
+    page: {
+      type: Number, value: 1, observer: "pageChange"
+    },
+    kw: {
+      type: String, value: '', observer: "kwChange"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    query: {},
+    loading: false,
+  },
+
+  ready: function(){
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    pageChange: function(){
+      console.log('page change')
+      var query = this.data.filter
+      query.kw = this.data.kw
+      query.page = this.data.page
+      this.setData({query: query})
+      this.loadData()
+    },
+
+    filterChange: function(){
+      console.log('filter change')
+      var query = this.data.filter || {}
+      query.page = 1
+      query.kw = this.data.kw
+      this.setData({query: query})
+      this.loadData()
+
+    },
+
+    kwChange: function(){
+      console.log('kw change')
+      var query = {}
+      query.page = 1
+      query.kw = this.data.kw
+      this.setData({query: query})
+      this.loadData()
+    },
+
+    loadData: function () {
+      var _this = this
+      var query = this.data.query
+      console.log('load data with query', query)
+
+      if (!query) {
+        return false
+      }
+      _this.setData({ loading: true })
+      var query = this.data.query
+      app.request({
+        url: '/api/v1/sub_districts/',
+        data: query,
+        complete: function (r) {
+        },
+        success: function (resp) {
+          var meta = resp.data.meta
+          var p = _this.data.query.page || 1
+          var i = p - 1
+          var data = { meta: meta, loading: false }
+          if (p > 1) {
+            var key = 'items[' + i + ']'
+            data[key] = resp.data.data
+          } else {
+            data['items'] = [resp.data.data]
+          }
+
+
+          _this.setData(data)
+
+          for(var i=0;i<=resp.data.data.length-1;i++){
+            var post = resp.data.data[i]
+            wx.setStorage({
+              key: 'sub_district.' + post.id,
+              data: post,
+            })
+          }
+        }
+      })
+    }
+  }
+})
---
+++ pages/sub-districts/show.js
@@ -0,0 +1,92 @@
+// pages/sub-districts/show.js
+const app = getApp()
+var WxParse = require('../../utils/wxParse/wxParse.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    htmlContent: '',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (q) {
+    this.setData({
+      id: q.id,
+      from_share: q.from_share || 0,
+      sub: wx.getStorageSync('subdistrict.' + q.id),
+      postQuery: {
+        sub_district_id: q.id,
+        
+      }
+    })
+    this.loadData()
+  },
+
+
+  loadData: function(){
+    var _this = this
+    app.request({
+      url: '/api/v1/sub_districts/' + _this.data.id,
+      success: function (resp) {
+        _this.setData({
+          sub: resp.data.data
+        })
+        WxParse.wxParse('htmlContent', 'html', resp.data.data.desc, _this, 5);
+        console.log('html', _this.data.htmlContent)
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
