bafd4aea243cc8da55386a19eec917dfcfa610db
TY@MacBook Pro <tianyu0915@gmail.com>
2019-05-18 22:22:59 +0800

    update

---
+++ components/icon-block.js
@@ -0,0 +1,32 @@
+// components/icon-block.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    width: {
+      type: Number, value: 80,
+    },
+    color: {
+      type: String, value: '#F8E71C'
+    },
+    name: {
+      type: String, value: 'like-o',
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
