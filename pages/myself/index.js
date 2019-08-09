67a0b2d655cde0e845d0f81befc116beb4985bb6
TY@MacBook Pro <tianyu0915@gmail.com>
2019-08-09 13:58:19 +0800

    bugfix

--- pages/myself/index.js
+++ pages/myself/index.js
@@ -305,6 +305,7 @@ Page({
           if(userInfo && userInfo.id){
             _this.setData({userInfo: userInfo})
             _this.getRemoteUserInfo()
+            console.log('uesr info', userInfo)
           }
         },
       })
