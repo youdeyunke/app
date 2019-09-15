dd5d57c63a2527f2f09fab57edb44096132479e8
TianYu <tianyu@mbp>
2019-09-15 12:07:59 +0800

    update balances info

--- pages/fenxiao/balance.js
+++ pages/fenxiao/balance.js
@@ -1,18 +1,48 @@
 // pages/fenxiao/balance.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      page: 1,
+      items: [],
+      per_page: 40,
+      loading: true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      this.loadData()
+  },
 
+  loadData: function(){
+      this.setData({loading: true})
+      var _this  = this
+      var query = {
+          page: _this.data.page,
+          per_page: _this.data.per_page,
+      }
+      app.request({
+          url: '/api/v1/balances/',
+          data: query,
+          success: function(resp){
+              var p = query['page']  - 1
+              var data = { loading: false }
+              if (p > 1) {
+                var key = 'items[' + i + ']'
+                data[key] = resp.data.data
+              } else {
+                data['items'] = [resp.data.data]
+              }
+              _this.setData(data)
+              console.log('data is', data, resp.data.data)
+          },
+      })
   },
 
   /**
@@ -47,14 +77,17 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+      this.setData({page: 1})
+      this.loadData()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+      var page = this.data.page
+      this.setData({page: page + 1})
+      this.loadData()
   },
 
   /**
