92d77d2dbbee10cf47a9bb734680b5f289c081cb
yohn <yohn@surface.com>
2020-01-14 21:31:33 +0800

    修改户型图不能点击放大的问题

--- pages/post/types-block.js
+++ pages/post/types-block.js
@@ -1,65 +1,81 @@
 // pages/post/types-block.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-      value: {type: Object }
-  },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        value: { type: Object }
+    },
 
-  observers: {
-      "value.groups": function(val){
-          this.setData({
-              currentGroup: val[0]
-          })
-      },
+    observers: {
+        "value.groups": function (val) {
+            this.setData({
+                currentGroup: val[0]
+            })
+        },
 
-      "currentGroup": function(val){
-          console.log('current group change', val)
-          var items = []
-          this.data.value.items.forEach((item, i) => {
-              item.className = ' '
-              if(item.s == val){
-                  items.push(item)
-              }
-          })
-          // 设置最后一个不显示border
-          var i = items.length - 1
-          items[i].className = 'border-none'
-            
-          // 标签发生变化
-          var tags  = []
-          this.data.value.groups.forEach((g, i) => {
-              var tag = {name: g + '室', group: g, className: ' '}
-              if(g == val){
-                  tag.className = 'active'
-              }
-              tags.push(tag)
-          })
-          this.setData({items: items, tags: tags})
-      },
-  },
+        "currentGroup": function (val) {
+            console.log('current group change', val)
+            var items = []
+            var imgUrls = []
+            this.data.value.items.forEach((item, i) => {
+                item.className = ' '
+                if (item.s == val) {
+                    items.push(item)
+                }
+                if (item.image) {
+                    imgUrls.push(item.image.url)
+                }
+            })
+            // 设置最后一个不显示border
+            var i = items.length - 1
+            items[i].className = 'border-none'
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-      items: [],
-      tags: [],
-      currentGroup: 0,
-  },
+            // 标签发生变化
+            var tags = []
+            this.data.value.groups.forEach((g, i) => {
+                var tag = { name: g + '室', group: g, className: ' ' }
+                if (g == val) {
+                    tag.className = 'active'
+                }
+                tags.push(tag)
+            })
+            this.setData({ items: items, tags: tags, imgUrls: imgUrls })
+        },
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-      groupClick: function(e){
-          var g = e.target.dataset['group']
-          if(g == this.data.currentGroup){
-              return false
-          }
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        items: [],
+        tags: [],
+        imgUrls: [],
+        currentGroup: 0,
+    },
 
-          this.setData({currentGroup: g})
-      },
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        viewImage: function (e) {
+            var i = e.currentTarget.dataset.index
+            var item = this.data.items[i]
+            var url = item.image.url
+            var urls = this.data.imgUrls
+            wx.previewImage({
+                current: url,
+                urls: urls,
+            })
+        },
+
+        groupClick: function (e) {
+            var g = e.target.dataset['group']
+            if (g == this.data.currentGroup) {
+                return false
+            }
+
+            this.setData({ currentGroup: g })
+        },
+    }
 })
