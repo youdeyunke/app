dc53368979f0db581c0b2fb9843886fdba83d8de
yohn <yohn@surface.com>
2020-02-06 13:10:49 +0800

    修改了nav按钮的跳转方式

--- components/navs.js
+++ components/navs.js
@@ -2,60 +2,80 @@
 const app = getApp()
 
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-      items: {type: Array, value: []},
-  },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        items: { type: Array, value: [] },
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-      navPages: wx.getStorageSync('navPages'),
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        navPages: wx.getStorageSync('navPages'),
+    },
 
-  observers: {
-      "items.**": function(val){
-          this.initNavs()
-      },
-  },
+    observers: {
+        "items.**": function (val) {
+            this.initNavs()
+        },
+    },
 
-  ready: function(){
-  },
+    ready: function () {
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    initNavs: function(){
-      var _this = this
-      var navs = this.data.items
-      var navPages = []
-      var index = 0
-      var pageSize = 10
-      for(var i=0;i<=navs.length -1;i++){
-        var nav = navs[i]
-        if (!nav.path.startsWith('/')) {
-          nav.path = '/' + nav.path
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        clickHandle: function (e) {
+            var page = e.currentTarget.dataset.page
+            var index = e.currentTarget.dataset.index
+            console.log('p', page, 'i', index)
+            var nav = this.data.navPages[page][index]
+            console.log('nav click', nav)
+            switch (nav.opentype.toLowerCase()) {
+                case 'navigateto':
+                    wx.navigateTo({
+                        url: nav.path
+                    })
+                    break;
+                case 'switchtab':
+                    wx.switchTab({ url: nav.path })
+                    break;
+                case 'miniprogram':
+                    console.log('打开小程序')
+                    break;
+            }
+        },
+        initNavs: function () {
+            var _this = this
+            var navs = this.data.items
+            var navPages = []
+            var index = 0
+            var pageSize = 10
+            for (var i = 0; i <= navs.length - 1; i++) {
+                var nav = navs[i]
+                if (!nav.path.startsWith('/')) {
+                    nav.path = '/' + nav.path
+                }
+
+                if (navPages[index] && navPages[index].length == pageSize) {
+                    // 满了
+                    index += 1
+                }
+
+                // 将navs 转换成可翻页的格式
+                if (typeof navPages[index] == 'undefined') {
+                    navPages[index] = [nav]
+                } else {
+                    navPages[index].push(nav)
+                }
+
+            }
+            console.log('navpages ', navPages)
+            _this.setData({ navPages: navPages })
         }
-
-        if(navPages[index] && navPages[index].length == pageSize){
-            // 满了
-            index += 1
-        }
-
-        // 将navs 转换成可翻页的格式
-        if(typeof navPages[index] == 'undefined'){
-            navPages[index] = [nav]
-        }else{
-          navPages[index].push(nav)
-        }
-        
-      }
-      console.log('navpages ', navPages)
-      _this.setData({navPages: navPages})
     }
-  }
 })
