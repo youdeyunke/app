46b3f7264b12bbd84feaf3137c214a4e1375c706
yohn <yohn@surface.com>
2020-02-12 16:22:21 +0800

    bugfix for news page loading

--- pkgNews/pages/news/index.js
+++ pkgNews/pages/news/index.js
@@ -3,178 +3,180 @@ const app = getApp()
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    page : 1,
-    catId: '',
-    per_page: 8,
-    active: 0,
-    isEmpty: false,
-    isEnd: false,
-    loading: false,
-    cats_key : 'news.cats',
-    news: [],
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        page: 1,
+        catId: '',
+        per_page: 8,
+        active: 0,
+        isEmpty: false,
+        isEnd: false,
+        loading: false,
+        cats_key: 'news.cats',
+        news: [],
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    app.checkForceLogin()
-    var _this = this
-    wx.setNavigationBarTitle({title: '头条'})
-    this.ensureCats(function (cats) {
-      var data = { cats: cats }
-      _this.setData(data)
-      _this.loadNews()
-    })    
-  },
-
-  onShow: function(){
-    this.loadCats()
-  },
-
-  catChange: function(e){
-     var i = e.detail.name
-     var cat = this.data.cats[i]
-     this.setData({
-         catId: cat.id,
-         page: 1,
-         news: [],
-     })
-     this.loadNews()
-  },
-
-
-  ensureCats: function(cb){
-    var key = this.data.cats_key
-    var cats = wx.getStorageInfoSync(key)
-    if(cats && cats.length > 0){
-      return cb(cats)
-    }else{
-      this.loadCats(function(cats){
-        return cb(cats)
-      })
-    }
-  },
-
-  loadCats: function(cb){
-    var _this = this
-    app.request({
-      hideLoading: true,
-      url: '/api/v1/news_cats/',
-      hideLoading: true,
-      success: function(resp){
-        _this.setData({
-          cats: resp.data.data,
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        app.checkForceLogin()
+        var _this = this
+        wx.setNavigationBarTitle({ title: '头条' })
+        this.ensureCats(function (cats) {
+            var data = { cats: cats }
+            _this.setData(data)
+            _this.loadNews()
         })
-        var key = _this.data.cats_key
-        wx.setStorageSync(key, resp.data.data)
-                
-        if(typeof cb == 'function'){
-          return cb(resp.data.data)
+    },
+
+    onShow: function () {
+        this.loadCats()
+    },
+
+    catChange: function (e) {
+        var i = e.detail.name
+        var cat = this.data.cats[i]
+        this.setData({
+            isEmpty: false,
+            catId: cat.id,
+            page: 1,
+            news: [],
+        })
+        this.loadNews()
+    },
+
+
+    ensureCats: function (cb) {
+        var key = this.data.cats_key
+        var cats = wx.getStorageInfoSync(key)
+        if (cats && cats.length > 0) {
+            return cb(cats)
+        } else {
+            this.loadCats(function (cats) {
+                return cb(cats)
+            })
         }
-      }
-    })
-  },
+    },
 
-  loadNews: function(){
-    var _this = this
-    this.setData({
-      isEmpty: false
-    })
-    
-    if (this.data.isEnd) {
-      return false
-    }  
+    loadCats: function (cb) {
+        var _this = this
+        app.request({
+            hideLoading: true,
+            url: '/api/v1/news_cats/',
+            hideLoading: true,
+            success: function (resp) {
+                _this.setData({
+                    cats: resp.data.data,
+                })
+                var key = _this.data.cats_key
+                wx.setStorageSync(key, resp.data.data)
 
-    this.setData({
-      loading: true
-    })
-    var query = {
-      cat_id: _this.data.catId,
-      page: _this.data.page, 
-      per_page : _this.data.per_page}
-    app.request({
-      url: '/api/v1/news',
-      data: query,
-      hideLoading: true,
-      success: function(resp){
-        var index = _this.data.page - 1
-        var key = 'news[' + index + ']'
-        var data = {
-          loading: false,
+                if (typeof cb == 'function') {
+                    return cb(resp.data.data)
+                }
+            }
+        })
+    },
+
+    loadNews: function () {
+        var _this = this
+        this.setData({
+            isEmpty: false
+        })
+
+        if (this.data.isEnd) {
+            return false
         }
 
-        if(resp.data.data.length == 0){
-          data['isEnd'] = true
-          if(_this.data.page == 1){
-            data['isEmpty'] = true
-            data['isEnd'] = false
-          }
+        this.setData({
+            loading: true
+        })
+        var query = {
+            cat_id: _this.data.catId,
+            page: _this.data.page,
+            per_page: _this.data.per_page
         }
+        app.request({
+            url: '/api/v1/news',
+            data: query,
+            hideLoading: true,
+            success: function (resp) {
+                var index = _this.data.page - 1
+                var key = 'news[' + index + ']'
+                var data = {
+                    loading: false,
+                }
 
-        data[key] = resp.data.data
-        _this.setData(data)
-        console.log('set data', data)
-      }
-    })
-  },
-  
+                if (resp.data.data.length == 0) {
+                    data['isEnd'] = true
+                    if (_this.data.page == 1) {
+                        data['isEmpty'] = true
+                        data['isEnd'] = false
+                    }
+                }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+                data[key] = resp.data.data
+                _this.setData(data)
+                console.log('set data', data)
+            }
+        })
+    },
 
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    var _this = this
-    this.setData({
-      news: [],
-      isEmpty: false,
-      isEnd: false,
-      page: 1,
-    })  
-    this.loadNews()
-  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    var _this = this
-    this.setData({
-      page: _this.data.page + 1
-    })
-    this.loadNews()
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+        var _this = this
+        this.setData({
+            news: [],
+            isEmpty: false,
+            isEnd: false,
+            page: 1,
+        })
+        this.loadNews()
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+        var _this = this
+        this.setData({
+            page: _this.data.page + 1
+        })
+        this.loadNews()
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
