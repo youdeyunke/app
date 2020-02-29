6ded313f8da46bf4207efc8618d00009352b45e5
yohn <yohn@surface.com>
2020-02-29 15:14:53 +0800

    优化了活动详情页面

--- pkgTour/pages/tour/show.js
+++ pkgTour/pages/tour/show.js
@@ -114,7 +114,9 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-        wx.hideLoading()
+        setTimeout(function () {
+            wx.hideLoading()
+        }, 500)
     },
 
     /**
