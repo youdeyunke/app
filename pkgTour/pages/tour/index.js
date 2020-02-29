c27640de6a7bacc0cdbdcc90d24b00ddc2e55c2a
yohn <yohn@surface.com>
2020-02-29 14:28:13 +0800

    增加了开放团页面

---
+++ pkgTour/pages/tour/index.js
@@ -0,0 +1,119 @@
+// pages/news/index.js
+const app = getApp()
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        page: 1,
+        per_page: 10,
+        active: 0,
+        loading: false,
+        items: [],
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (q) {
+        app.checkForceLogin()
+        var _this = this
+        var catId = q.cat_id || ''
+        wx.setNavigationBarTitle({ title: '看房团活动' })
+        this.loadData()
+    },
+
+    onShow: function () {
+    },
+
+    loadData: function () {
+        var _this = this
+        this.setData({
+            loading: true
+        })
+        var query = {
+            page: _this.data.page,
+            per_page: _this.data.per_page
+        }
+        app.request({
+            url: '/api/v1/tours',
+            data: query,
+            hideLoading: true,
+            success: function (resp) {
+                var index = _this.data.page - 1
+                var key = 'items[' + index + ']'
+                var data = {
+                    loading: false,
+                }
+
+                if (resp.data.data.length == 0) {
+                    data['isEnd'] = true
+                    if (_this.data.page == 1) {
+                        data['isEmpty'] = true
+                        data['isEnd'] = false
+                    }
+                }
+
+                data[key] = resp.data.data
+                _this.setData(data)
+            }
+        })
+    },
+
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+        var _this = this
+        this.setData({
+            items: [],
+            isEmpty: false,
+            isEnd: false,
+            page: 1,
+        })
+        this.loadData()
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+        var _this = this
+        this.setData({
+            page: _this.data.page + 1
+        })
+        this.loadData()
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ pkgTour/pages/tour/show.js
@@ -0,0 +1,150 @@
+// pages/news/show.js
+const app = getApp()
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        tourId: null,
+        homebtn: null,
+        joined: false,
+        html: '',
+        item: null,
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (q) {
+        app.checkForceLogin()
+        this.setData({ tourId: q.id })
+        this.loadData()
+    },
+
+    joinHandle: function (e) {
+        if (!this.data.item.status.value == 1) {
+            return false
+        }
+
+        this.setData({ joinLoading: true })
+        var _this = this
+        var url = '/api/v1/tour_members/'
+        var data = { tour_id: this.data.item.id }
+        var method = 'POST'
+        var joined = this.data.joined
+        if (joined == true) {
+            url = '/api/v1/tour_members/0'
+            method = 'DELETE'
+        }
+        app.request({
+            url: url,
+            method: method,
+            data: data,
+            success: function (resp) {
+                if (resp.data.status == 0) {
+                    _this.loadData((res) => {
+                        console.log('cb res is', res)
+                        if (res.joined) {
+                            wx.showModal({
+                                title: '报名成功',
+                                content: '您已成功报名此次活动，我们的工作人员将在活动开始前与您来电确认，请留意接听。',
+                                showCancel: false,
+                                confirmText: '知道了',
+                                confirmColor: '#1989fa',
+                                success: (result) => {
+                                    if (result.confirm) {
+                                    }
+                                },
+                                fail: () => { },
+                                complete: () => { }
+                            });
+
+                        }
+                    })
+                    _this.setData({ joinLoading: false })
+                }
+            }
+        })
+
+    },
+
+    loadData: function (cb) {
+        var _this = this
+        app.request({
+            url: '/api/v1/tours/' + _this.data.tourId,
+            success: function (resp) {
+                var tour = resp.data.data.tour
+                var joined = resp.data.data.joined
+                var html = tour['content'] || ''
+                console.log('tour', tour, 'join', joined)
+                if (html) {
+                    html = html.replace(/\<img/gi, '<img class="rich-text-img" ')
+                    html = html.replace(/\<p/gi, '<p class="rich-text-p" ')
+                }
+                _this.setData({
+                    item: tour,
+                    html: html,
+                    loading: false,
+                    joined: joined
+                })
+                return typeof cb == 'function' && cb(resp.data.data)
+            }
+        })
+    },
+
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        wx.hideLoading()
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+        return {
+            title: this.data.item.title,
+            imageUrl: this.data.item.cover + "?imageView2/1/w/500/h/400",
+            path: '/pkgNews/pages/tours/show?id=' + this.data.tourId
+        }
+    }
+})
