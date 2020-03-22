ae47183964646defca2b08b314088298c1459e02
yohn <yohn@surface.com>
2020-03-22 16:01:10 +0800

    修复聊 首页头条不能跳转打开url的问题

--- components/news-swiper.js
+++ components/news-swiper.js
@@ -22,5 +22,23 @@ Component({
   /**
    * 组件的方法列表
    */
-  methods: { }
+    methods: { 
+        itemClick: function (e) {
+            var _this = this
+            var index = e.currentTarget.dataset.index
+            var item = this.data.items[index]
+            var url = item.url
+            var path = '/pkgNews/pages/news/show?id=' + item.id
+            // 判断是打开webview，还是显示自定义内容
+            if (url) {
+                console.log('goto webview', url)
+                app.gotoWebview(url, item.title)
+                return false
+            }
+            wx.navigateTo({ url: path, })
+
+        },
+
+
+  }
 })
