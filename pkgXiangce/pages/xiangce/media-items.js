ad52f0442b21fd7be7c6a86d44b35436100ef14a
yohn <yohn@surface.com>
2020-03-26 13:04:01 +0800

    新增楼盘相册功能

--- pkgXiangce/pages/xiangce/media-items.js
+++ pkgXiangce/pages/xiangce/media-items.js
@@ -19,13 +19,21 @@ Component({
      * 组件的方法列表
      */
     methods: {
+        viewImage: function (item) {
+            var url = item.url
+            var urls = this.data.items.map((m, i) => { return m.url })
+            wx.previewImage({
+                current: url,
+                urls: urls,
+            })
+        },
         itemClick: function (e) {
             var _this = this
             var index = e.currentTarget.dataset.index
             var item = this.data.items[index]
             switch (item.file_type) {
                 case 'image':
-                    _this.imageView(item)
+                    _this.viewImage(item)
                     break;
                 case 'video':
                     var url = item.url
