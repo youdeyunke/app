7df342473134aa7a00b85e271eb3ab9c0459592c
yohn <yohn@surface.com>
2020-03-26 11:53:28 +0800

    增加了单独的相册功能

---
+++ pkgXiangce/pages/xiangce/media-items.js
@@ -0,0 +1,39 @@
+// pkgXiangce/pages/xiangce/index/media-items.js
+const app = getApp()
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        items: { type: Object, value: [] }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        itemClick: function (e) {
+            var _this = this
+            var index = e.currentTarget.dataset.index
+            var item = this.data.items[index]
+            switch (item.file_type) {
+                case 'image':
+                    _this.imageView(item)
+                    break;
+                case 'video':
+                    var url = item.url
+                    app.gotoVideo(url, '视频')
+                    break;
+            }
+
+        },
+
+    }
+})
