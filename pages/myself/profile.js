2928ff6934792e603ca785ec6a751db70b56516a
yohn <yohn@surface.com>
2020-05-09 11:17:53 +0800

    update

--- pages/myself/profile.js
+++ pages/myself/profile.js
@@ -5,134 +5,184 @@ var qiniu = require('../../utils/qiniu.js');
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-      loading: true,
-      userInfo: { },
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        loading: true,
+        userInfo: {},
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (q) {
-      wx.setNavigationBarTitle({ title: '修改个人信息' })        
-      this.loadUser()
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (q) {
+        wx.setNavigationBarTitle({ title: '修改个人信息' })
+        this.loadUser()
+    },
 
 
-  goBack: function(e){
-      wx.navigateBack({ delta: -1 })
-  },
+    goBack: function (e) {
+        wx.navigateBack({ delta: -1 })
+    },
 
-  submitHandle: function(e){
-    var _this = this
-    app.uploadFormid(e)
-    var data = e.detail.value
-    app.request({
-        url: '/api/v1/users/0',
-        method: 'PUT',
-        data: {user: data},
-        success: function(resp){
-            if(resp.data.status == 0){
-                wx.showToast({ icon: 'none', title: '修改成功', success: function(){
-                    wx.navigateBack({delta: -1})
-                }})
+    submitHandle: function (e) {
+        var _this = this
+        var data = this.data.userInfo
+        app.request({
+            url: '/api/v1/users/0',
+            method: 'PUT',
+            data: { user: data },
+            success: function (resp) {
+                if (resp.data.status == 0) {
+                    wx.showToast({
+                        icon: 'none', title: '修改成功', success: function () {
+                            wx.navigateBack({ delta: -1 })
+                        }
+                    })
+                }
             }
-        }
-    })
-  },
+        })
+    },
 
-  chooseImage: function(e){
-      var _this = this
-      wx.chooseImage({
-        count: 1,
-        sizeType: ['original', 'compressed'],
-        success (res) {
-          const path = res.tempFilePaths[0]
-          qiniu.upload(path, (url) => {
-              _this.updateAvatar(url)
-          })
-        }
-    })
-  },
+    chooseImage: function (e) {
+        var _this = this
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original', 'compressed'],
+            success(res) {
+                const path = res.tempFilePaths[0]
+                qiniu.upload(path, (url) => {
+                    _this.updateAvatar(url)
+                })
+            }
+        })
+    },
 
-  updateAvatar: function(url){
-      var _this = this
-      // 设置avatar
-      app.request({
-        url: '/api/v1/users/update_avatar', 
-        data: {avatar: url},
-        method: 'POST',
-        success: function(resp){
-          if(resp.data.status == 0){
-            _this.loadUser()
-            wx.showToast({
-              icon: 'none',
-              title: '头像上传成功！',
-              duration: 2000,
-            })
-          }
-        }
-      })
-  },
+    chooseQrImage: function (e) {
+        // 上传微信二维码
+        var _this = this
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original', 'compressed'],
+            success(res) {
+                const path = res.tempFilePaths[0]
+                qiniu.upload(path, (url) => {
+                    _this.updateWechatQr(url)
+                })
+            }
+        })
+    },
+
+    updateWechatQr: function (url) {
+        var data = { wechat_qr: url }
+        var _this = this
+        app.request({
+            url: '/api/v1/users/0',
+            method: 'PUT',
+            data: { user: data },
+            success: function (resp) {
+                if (resp.data.status == 0) {
+                    var u = _this.data.userInfo
+                    u['wechat_qr'] = url
+                    _this.setData({ userInfo: u })
+                    wx.showToast({
+                        icon: 'none', title: '微信二维码已上传', success: function () {
+                        }
+                    })
+                }
+            }
+        })
+    },
+
+    switchChange: function (e) {
+        console.log('switch change', e)
+    },
+
+    textChange: function (e) {
+        console.log('e', e)
+        var k = e.currentTarget.dataset.name
+        var v = e.detail
+        var data = this.data.userInfo
+        data[k] = v
+        this.setData({ userInfo: data })
+    },
+
+    updateAvatar: function (url) {
+        var _this = this
+        // 设置avatar
+        app.request({
+            url: '/api/v1/users/update_avatar',
+            data: { avatar: url },
+            method: 'POST',
+            success: function (resp) {
+                if (resp.data.status == 0) {
+                    _this.loadUser()
+                    wx.showToast({
+                        icon: 'none',
+                        title: '头像上传成功！',
+                        duration: 2000,
+                    })
+                }
+            }
+        })
+    },
 
 
-  loadUser: function(){
-      this.setData({loading: true})
-      var _this = this
-      auth.getRemoteUserInfo(function(user) {
-        _this.setData({ userInfo: user, loading: false })
-       })
-  },
+    loadUser: function () {
+        this.setData({ loading: true })
+        var _this = this
+        auth.getRemoteUserInfo(function (user) {
+            _this.setData({ userInfo: user, loading: false })
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  }
+    }
 })
