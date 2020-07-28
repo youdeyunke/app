3b8863a979cbb53433d75a5111e3f96d9c96f644
yohn <yohn@surface.com>
2020-07-28 12:55:00 +0800

    优化了登陆页面

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -8,75 +8,77 @@ Page({
      * 页面的初始数据
      */
     data: {
-      loginMethod:'',
-      code: null,
-      loading:false,
+        loginMethod: '',
+        code: null,
+        loading: false,
     },
 
 
-  loginHandle: function(e){
-    var _this = this
-    // 注意，code 需要在getUserInfo之前获取到，否则会导致登录失败
-    // 2中登录方式，api/v1 : 账号授权登录    api/v2： 手机号授权登录
-    var code = _this.data.code
-    if (e.detail.errMsg != 'getUserInfo:ok' && e.detail.errMsg != 'getPhoneNumber:ok'){
-      wx.showModal({
-        content: '授权登录时出错：' + e.detail.errMsg,
-        showCancle: false
-      })
-      console.error('授权时候出错', e)
-      return
-    }
-
-    var encryptedData = e.detail.encryptedData
-    var iv = e.detail.iv
-  
-    // 换取服务器的token
-    this.setData({loading:true})
-    this.getSessionToken(code, encryptedData, iv, function(userInfo){
-      var pages = getCurrentPages()
-      if(pages.length == 1){
-        wx.switchTab({url: '/pages/myself/index'})
-      }else{
-       wx.navigateBack({delta: 1})
-      }
-    })
-  },
-
-
-  getSessionToken: function (code, encryptedData, iv, cb) {
-    // 重新获取token，并刷新 user info
-    var _this = this
-    var data =  { code: code, encryptedData: encryptedData, iv: iv }
-    data['referrer_id'] = wx.getStorageSync('referrer_id') || ''
-    // 如果有推荐人信息
-    if(data['referrer_id']){
-      console.log('检测到推荐人id：', data)
-    }
-
-    // 发送给服务器
-    app.request({
-      data: data,
-      method: 'POST',
-      url: '/api/' + _this.data.loginMethod + '/sessions',
-      hideLoading: true,
-      success: function (resp) {
-        var data = resp.data
-        if (data.status == 0) {
-          // 保存下服务器返回的token
-          var token = data.data.token
-          var user = data.data.user
-          wx.setStorageSync('token', token)
-          wx.setStorageSync('userInfo', user)
-          // 在globalData中标记登录状态
-          app.globalData.token = token
-          app.globalData.userInfo = user
-          // callback
-          typeof cb == "function" && cb(user)
+    loginHandle: function (e) {
+        var _this = this
+        // 注意，code 需要在getUserInfo之前获取到，否则会导致登录失败
+        // 2中登录方式，api/v1 : 账号授权登录    api/v2： 手机号授权登录
+        var code = _this.data.code
+        if (e.detail.errMsg != 'getUserInfo:ok' && e.detail.errMsg != 'getPhoneNumber:ok') {
+            console.log(e.detail.errMsg)
+            wx.showModal({
+                title: '操作失败',
+                content: '请允许微信授权，才能正常登陆账号哦',
+                showCancle: false
+            })
+            console.error('授权时候出错', e)
+            return
         }
-      }
-    })
-  },
+
+        var encryptedData = e.detail.encryptedData
+        var iv = e.detail.iv
+
+        // 换取服务器的token
+        this.setData({ loading: true })
+        this.getSessionToken(code, encryptedData, iv, function (userInfo) {
+            var pages = getCurrentPages()
+            if (pages.length == 1) {
+                wx.switchTab({ url: '/pages/myself/index' })
+            } else {
+                wx.navigateBack({ delta: 1 })
+            }
+        })
+    },
+
+
+    getSessionToken: function (code, encryptedData, iv, cb) {
+        // 重新获取token，并刷新 user info
+        var _this = this
+        var data = { code: code, encryptedData: encryptedData, iv: iv }
+        data['referrer_id'] = wx.getStorageSync('referrer_id') || ''
+        // 如果有推荐人信息
+        if (data['referrer_id']) {
+            console.log('检测到推荐人id：', data)
+        }
+
+        // 发送给服务器
+        app.request({
+            data: data,
+            method: 'POST',
+            url: '/api/' + _this.data.loginMethod + '/sessions',
+            hideLoading: true,
+            success: function (resp) {
+                var data = resp.data
+                if (data.status == 0) {
+                    // 保存下服务器返回的token
+                    var token = data.data.token
+                    var user = data.data.user
+                    wx.setStorageSync('token', token)
+                    wx.setStorageSync('userInfo', user)
+                    // 在globalData中标记登录状态
+                    app.globalData.token = token
+                    app.globalData.userInfo = user
+                    // callback
+                    typeof cb == "function" && cb(user)
+                }
+            }
+        })
+    },
 
 
     /**
@@ -106,21 +108,21 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-      var _this = this
-      this.setData({ loading: false })
+        var _this = this
+        this.setData({ loading: false })
 
-      wx.login({
-        success: function(res){
-          console.log('login code', res.code)
-          _this.setData({code: res.code})
-        },
-        complete: function (res) {
-            // 用户拒绝,跳转到设置界面
-            if (res.errMsg == 'getUserInfo:fail auth deny') {
-              wx.openSetting({})
+        wx.login({
+            success: function (res) {
+                console.log('login code', res.code)
+                _this.setData({ code: res.code })
+            },
+            complete: function (res) {
+                // 用户拒绝,跳转到设置界面
+                if (res.errMsg == 'getUserInfo:fail auth deny') {
+                    wx.openSetting({})
+                }
             }
-        }
-      })
+        })
     },
 
     /**
