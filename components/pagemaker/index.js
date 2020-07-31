42156d7fedd8b301b5bf5e36b6e86370b7b00d10
yohn <yohn@surface.com>
2020-07-31 11:58:03 +0800

    update

--- components/pagemaker/index.js
+++ components/pagemaker/index.js
@@ -14,6 +14,7 @@ Component({
      */
     data: {
         modules: [],
+        config: null,
     },
 
 
@@ -25,10 +26,11 @@ Component({
             success: function (resp) {
                 var data = resp.data.data
                 _this.setData({
-                    modules: data.modules
+                    modules: data.modules,
+                    pageConfig: data.config,
                 })
                 _this.triggerEvent('ready', data.config)
-
+                _this.setNavbar(data.config)
             }
         })
 
@@ -38,6 +40,24 @@ Component({
      * 组件的方法列表
      */
     methods: {
+        setNavbar: function (config) {
+            // 设置导航栏颜色、文字、背景
+            var bgColor = config.title.bgColor
+            var fontColor = config.title.color
+            var title = config.title.value
+            console.log('bg', bgColor, 'font', fontColor)
+            wx.setNavigationBarColor({
+                frontColor: fontColor,
+                backgroundColor: bgColor,
+                success: (err) => {
+                    console.log('set nav error', err)
+                },
+            });
+
+            wx.setNavigationBarTitle({
+                title: title,
+            });
+        }
 
     }
 })
