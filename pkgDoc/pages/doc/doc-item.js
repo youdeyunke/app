a8c26a414bca100f136d83ccceba684599e7c4a9
unknown <you@example.com>
2020-10-20 17:49:11 +0800

    '楼盘资料优化'

--- pkgDoc/pages/doc/doc-item.js
+++ pkgDoc/pages/doc/doc-item.js
@@ -17,19 +17,45 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    openDocument(){
+    showFile(){
+      wx.showLoading({title: '加载中',mask:'true'})
+      var _this = this
+      wx.getStorage({
+        key: "doc."+_this.data.item.id,
+        success (res) {
+          wx.openDocument({
+            filePath: res.data,
+            fileType:_this.data.item.file_type,
+            success(){
+              wx.hideLoading()
+            }
+          })
+        },
+        fail(){
+          _this.downloadFile()
+        }
+      })
+    },
+    downloadFile(){
       var _this = this
       wx.downloadFile({
-        // 示例 url，并非真实存在
         url: _this.data.item.url,
         success: function (res) {
           const filePath = res.tempFilePath
+          wx.setStorage({
+            key:"doc."+_this.data.item.id,
+            data:filePath
+          })
           wx.openDocument({
             filePath: filePath,
             fileType:_this.data.item.file_type,
+            success(){
+              wx.hideLoading()
+            }
           })
         }
       })
     },
+
   }
 })
