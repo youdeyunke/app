4ca75d6c536814e66e9f0a045533661fae259b4d
unknown <you@example.com>
2020-11-19 16:36:38 +0800

    '报备客户页面优化'

--- pkgFenxiao/pages/fenxiao/report.js
+++ pkgFenxiao/pages/fenxiao/report.js
@@ -31,9 +31,9 @@ Page({
             value: 'full'
         },
         ],
-
+        name:'',
+        mobile:'',
         mobileType: 'protected',
-
         setpsText: ["报备客户", "核实成交", "发放佣金"],
         popupShow:false
     },
