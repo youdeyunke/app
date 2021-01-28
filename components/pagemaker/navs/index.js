36e19327733fdcb6c00ccd6af277d0bc05437639
yohn <yohn@surface.com>
2021-01-28 16:25:45 +0800

    优化了pagemaker中的navs组件’

--- components/pagemaker/navs/index.js
+++ components/pagemaker/navs/index.js
@@ -7,10 +7,27 @@ Component({
         config: { type: Object, default: null },
     },
 
+    observers: {
+        "config.items": function (items) {
+            var navs = items.map((item, i) => {
+                if (item.title.includes('-')) {
+                    var r = item.title.split('-')
+                    item.showCount = true
+                    item.count = r[1]
+                    item.title = r[0]
+                }
+                return item
+            })
+            console.log('navs is', navs)
+            this.setData({ navs: navs })
+        },
+    },
+
     /**
      * 组件的初始数据
      */
     data: {
+        navs: [],
 
     },
 
