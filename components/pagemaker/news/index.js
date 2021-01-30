89c8ff1bf9e5dd540a324f7180cee6fc04d6516c
yohn <yohn@surface.com>
2021-01-30 14:17:37 +0800

    初步实现了news模块，数据库需要修改

--- components/pagemaker/news/index.js
+++ components/pagemaker/news/index.js
@@ -1,23 +1,54 @@
 // components/pagemaker/news/index.js
+const app = getApp()
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        config: { type: Object, default: null }
 
-  },
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    observers: {
+        "config.ids": function (ids) {
+            this.loadNews(ids)
+        }
+    },
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+        items: []
+    },
 
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        loadNews: function (ids) {
+            if (!ids) {
+                return
+            }
+            if (ids.length == 0) {
+                return
+            }
+            var query = ids.join(',')
+            var _this = this
+            app.request({
+                url: '/api/v1/news',
+                data: query,
+                hideLoading: true,
+                success: function (resp) {
+                    if (resp.data.status == 0) {
+                        var items = resp.data.data
+                        console.log('load items is', items)
+                        _this.setData({ items: items })
+                    }
+                }
+            })
+        }
+
+    }
 })
