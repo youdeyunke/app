99a2da5f2c73356bbdbec2fafe247d5de39607a2
yohn <yohn@surface.com>
2021-02-19 17:05:28 +0800

    update post page

--- components/ud/image/index.js
+++ components/ud/image/index.js
@@ -3,7 +3,7 @@ Component({
     /**
      * 组件的属性列表
      */
-    extrenalClasses: ['custom-class'],
+    externalClasses: ['custom-class'],
     properties: {
         src: { type: String, value: null },
         mode: { type: String, value: "fit" },
