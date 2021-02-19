ab23f6d5f6e02130738e0f1804ed2fd8336c97a1
yohn <yohn@surface.com>
2021-02-19 16:55:22 +0800

    修改了assets

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,46 +1,46 @@
 Component({
-  data: {
-    selected: 0,
-    selectedColor: "#333",
-    backgroundColor: "#fff",
-    color: "#999",
-    borderStyle: "black",
-    list: [{
-      pagePath: "/pages/home/home",
-      iconPath: "/assets/icons/home.0.png",
-      selectedIconPath: "/assets/icons/home.1.png",
-      text: "首页"
-    },
-    {
-      pagePath: "/pages/messages/index",
-      iconPath: "/assets/icons/message.0.png",
-      selectedIconPath: "/assets/icons/message.1.png",
-      text: "消息"
-    },
-    {
-      pagePath: "/pages/faxian/index",
-      iconPath: "/assets/icons/faxian.0.png",
-      selectedIconPath: "/assets/icons/faxian.1.png",
-      text: "发现"
-    },
+    data: {
+        selected: 0,
+        selectedColor: "#333",
+        backgroundColor: "#fff",
+        color: "#999",
+        borderStyle: "black",
+        list: [{
+            pagePath: "/pages/home/home",
+            iconPath: "/assets/tabs/home.0.png",
+            selectedIconPath: "/assets/tabs/home.1.png",
+            text: "首页"
+        },
+        {
+            pagePath: "/pages/messages/index",
+            iconPath: "/assets/tabs/message.0.png",
+            selectedIconPath: "/assets/tabs/message.1.png",
+            text: "消息"
+        },
+        {
+            pagePath: "/pages/faxian/index",
+            iconPath: "/assets/tabs/faxian.0.png",
+            selectedIconPath: "/assets/tabs/faxian.1.png",
+            text: "发现"
+        },
 
-    {
-      pagePath: "/pages/myself/index",
-      iconPath: "/assets/icons/myself.0.png",
-      selectedIconPath: "/assets/icons/myself.1.png",
-      text: "我的"
-    }]
-  },
-  attached() {
-  },
-  methods: {
-    switchTab(e) {
-      const data = e.currentTarget.dataset
-      const url = data.path
-      this.setData({
-        selected: data.index
-      })
-      wx.switchTab({url})
+        {
+            pagePath: "/pages/myself/index",
+            iconPath: "/assets/tabs/myself.0.png",
+            selectedIconPath: "/assets/tabs/myself.1.png",
+            text: "我的"
+        }]
+    },
+    attached() {
+    },
+    methods: {
+        switchTab(e) {
+            const data = e.currentTarget.dataset
+            const url = data.path
+            this.setData({
+                selected: data.index
+            })
+            wx.switchTab({ url })
+        }
     }
-  }
 })
