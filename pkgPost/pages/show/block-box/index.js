974a10668317adcd49f3efb66ca4e07f715dec62
yohn <yohn@surface.com>
2021-02-24 11:52:09 +0800

    修改了楼盘详情页的block-box组件

---
+++ pkgPost/pages/show/block-box/index.js
@@ -0,0 +1,53 @@
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title: { type: String, value: '标题' },
+        subTitle: { type: String, value: '' },
+        url: { type: String, value: null },
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+        actionHandle: function (e) {
+            console.log('url:', this.data.url, 'opentype', this.data.opentype)
+            if (!this.data.url) {
+                return false
+            }
+
+            var _this = this
+            var url = _this.data.url
+            switch (_this.data.opentype.toLowerCase()) {
+                case 'navigateto':
+                    wx.navigateTo({
+                        url: url,
+                    })
+                    break;
+                case "switchtab":
+                    wx.switchTab({
+                        url: url,
+                    })
+                    break;
+                case "webview":
+                    wx.navigateTo({
+                        url: '/pages/webview/webview?url=' + url,
+                    })
+                    console.log('hello')
+                    break;
+                default:
+                    console.log('default:', _this.data.opentype.toLowerCase, 'url', url)
+            }
+        },
+
+    }
+})
