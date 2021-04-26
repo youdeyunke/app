00adf8e8222b1208d1c6799b0225d9b0ce94c9c6
张文斌 <18710775553@qq.com>
2021-04-26 16:16:40 +0800

    客户报备详情页面

--- pkgFenxiao/pages/fenxiao/customer-detail.js
+++ pkgFenxiao/pages/fenxiao/customer-detail.js
@@ -1,4 +1,5 @@
 // pkgFenxiao/pages/fenxiao/customer-detail.js
+const app=getApp()
 Page({
 
   /**
@@ -7,60 +8,29 @@ Page({
   data: {
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      id:options.id 
+    },()=>{
+      this.loadPost()
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  loadPost:function(){
+    var _this=this
+    app.request({
+      url:'/api/v1/customers/'+_this.data.id,
+      methods:"GET",
+      success:function(res){
+        console.log(res)
+        _this.setData({
+          value:res.data.data
+        })
+        console.log(_this.data.value)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
