2844dc0418a6ea192db8bb868785112a423bbcf5
张文斌 <18710775553@qq.com>
2021-04-28 09:30:15 +0800

    修改页面

---
+++ components/choose-block/choose-block.js
@@ -0,0 +1,25 @@
+// components/choose-block/choose-block.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    item:String,
+    sex:String,
+    value:String,
+    index:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods:{
+  }
+})
