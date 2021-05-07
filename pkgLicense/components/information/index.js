d58aa3ad79af201625a7e9f3003666610ab2ff87
张文斌 <18710775553@qq.com>
2021-05-07 15:09:30 +0800

    预售证查询组件

---
+++ pkgLicense/components/information/index.js
@@ -0,0 +1,26 @@
+// pkgLicense/components/information/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    options:{
+      type:Array,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
