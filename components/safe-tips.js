1176df85db78c069f4907f00bb1489d9bcea25cb
张文斌 <18710775553@qq.com>
2021-05-25 12:00:55 +0800

    保存图片自动关闭

--- components/safe-tips.js
+++ components/safe-tips.js
@@ -4,28 +4,27 @@ Component({
    * 组件的属性列表
    */
   properties: {
-      cat: { type: String, value: ''}
+    cat: { type: String, value: ''}
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-      info: {
-          new: {
-              title: '',
-              desc: '',
-          },
-          old: {
-              title: '本平台承诺真实房源假一赔百元!',
-              desc: '',
-          },
-          rental: {
-              title: '',
-              desc: '',
-          },
-      }
-
+    info: {
+      new: {
+          title: '',
+          desc: '',
+      },
+      old: {
+          title: '本平台承诺真实房源假一赔百元!',
+          desc: '',
+      },
+      rental: {
+          title: '',
+          desc: '',
+      },
+    }
   },
 
   /**
