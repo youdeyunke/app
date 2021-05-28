94fca01e54cca2d4c7f9fececa14b5756ec55932
张文斌 <18710775553@qq.com>
2021-05-28 10:21:03 +0800

    card组件显示问题

--- components/safe-tips.js
+++ components/safe-tips.js
@@ -4,18 +4,13 @@ Component({
    * 组件的属性列表
    */
   properties: {
-<<<<<<< HEAD
     cat: { type: String, value: ''}
-=======
-     
->>>>>>> 277600c68cb3b19b813d00419ba3ecb6794c9a6a
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-<<<<<<< HEAD
     info: {
       new: {
           title: '',
@@ -30,10 +25,6 @@ Component({
           desc: '',
       },
     }
-=======
-
-
->>>>>>> 277600c68cb3b19b813d00419ba3ecb6794c9a6a
   },
 
   /**
