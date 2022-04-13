30aca79cc9f0b7de92f6605bbac3d3b5be456315
xujinyu <1500730439@qq.com>
2022-04-13 16:37:01 +0800

    报备记录管理功能实现

---
+++ pkgFenxiao/pages/fenxiao/edit.js
@@ -0,0 +1,153 @@
+// pkgFenxiao/pages/fenxiao/edit.js
+
+const app = getApp()
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        id: null,
+        radio:'wuxiao',
+        jiaoyiRadio: '',
+        status: 0,
+        status_remark_items: ['超过报备保护期','客户信息填写错误','关联房源选择错误','其他原因'], 
+        deal_status_items: [],
+        deal_status_item_id: '',
+        status_remark: '',
+        receiver_name: '',
+        receiver_mobile: '',
+        admin_remark: ''
+    },
+
+    wuxiaoClick(e){
+        var i = e.currentTarget.dataset.i
+        console.log(e.currentTarget.dataset.i)
+        this.setData({
+            status_remark: this.data.status_remark_items[i]
+        })
+    },
+
+    radioChange(event){
+        this.setData({
+            radio: event.detail
+        })
+        if(event.detail == 'wuxiao'){
+            this.setData({
+                status: 0
+            })
+        }
+        if(event.detail == 'youxiao'){
+            this.setData({
+                status: 2
+            })
+        }
+    },
+
+    onClick(event){
+        console.log('222',event.currentTarget.dataset)
+        this.setData({
+            jiaoyiRadio: event.currentTarget.dataset.name,
+            deal_status_item_id: event.currentTarget.dataset.i+1
+        })
+    },
+
+    submitData(){
+        var id = this.data.id
+        var status = this.data.status
+        if(status == 0){
+            var data = {
+                status: this.data.status,
+                status_remark: this.data.status_remark,
+            }
+        }
+        if(status == 2){
+            var data = {
+                status: this.data.status,
+                deal_status_item_id: this.data.deal_status_item_id,
+                receiver_name: this.data.receiver_name,
+                receiver_mobile: this.data.receiver_mobile,
+                admin_remark: this.data.admin_remark
+            }
+        }
+        console.log(data)
+        app.request({
+            url: '/api/v1/customers/'+id,
+            method: 'PUT',
+            data: data,
+            success(res){
+                console.log(res)
+            }
+        })
+        wx.navigateBack({
+            delta: 1,
+          })
+    },
+
+    cancel(){
+        wx.navigateBack({
+          delta: 1,
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        console.log(options)
+        console.log(app.globalData.myconfigs.deal_status_items)
+        this.setData({
+            deal_status_items: app.globalData.myconfigs.deal_status_items,
+            id: options.id
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
