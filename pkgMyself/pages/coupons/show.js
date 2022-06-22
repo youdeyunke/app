fa7fe7323f7dd6013fd7509fa231265171680670
yohn <yohn@surface.com>
2022-06-22 10:35:43 +0800

    优化了领取优惠券功能

--- pkgMyself/pages/coupons/show.js
+++ pkgMyself/pages/coupons/show.js
@@ -25,62 +25,21 @@ Page({
 
   },
 
+  qrPreview: function(){
+    var url = this.data.coupon.qr  
+    wx.previewImage({
+      urls: [url],
+      current: url,
+    })
+  },
+
   callHandle: function(){
     wx.makePhoneCall({
       phoneNumber: this.data.coupon.shop_phone,
     })
   },
 
-  useHandle: function() {
-    if(this.data.coupon.cat != 'coupon'){
-      return 
-    }
-    this.setData({ 
-      showForm: true, 
-    })
-  },
 
-  formClose: function(){
-    this.setData({ 
-      showForm: false,
-      loading: false, 
-      password: '',
-    })
-  },
-
-  submitHandle: function(){
-    // 提交密码核销
-    var _this = this  
-    if(this.data.password.length != 4){
-      wx.showToast({
-        icon: 'none',
-        title: '请输入4位核销口令',
-      })
-      return false 
-    }
-    var data = {
-      password: this.data.password, 
-    }
-    app.request({ 
-      url: '/api/v1/coupons/' + this.data.cid, 
-      method: 'PUT', 
-      data: data, 
-      success: function(resp){
-        _this.formClose()
-
-        if(resp.data.status != 0){
-          return 
-        }
-        // success 
-        wx.showToast({
-          title: '已核销',
-        })
-        setTimeout(() => { 
-          _this.loadData()
-        }, 1500)
-       }
-    })
-  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -116,20 +75,24 @@ Page({
   loadData: function(){
     var _this = this  
     app.request({  
+      hideLoading: true, 
       url: '/api/v1/coupons/' + this.data.cid, 
       success: function(resp){ 
         if(resp.data.status != 0){
           return 
         }
         var c = resp.data.data  
-
-
         _this.setData({  
           coupon: c
         })
         wx.setNavigationBarTitle({
           title:  c.name,
         })
+        if(c.used == false ){        
+          setTimeout(() => { 
+            _this.loadData()
+          }, 2000)
+        }
       }
     })
   },
