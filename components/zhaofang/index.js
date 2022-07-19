9d9bc2fa6e6bd3145db5c0c373f7b984209f6602
xujinyu <1500730439@qq.com>
2022-07-19 14:06:59 +0800

    一键找房组件完成

--- components/zhaofang/index.js
+++ components/zhaofang/index.js
@@ -1,5 +1,6 @@
 // components/zhaofang/index.js
 const app = getApp()
+var auth = require('../../utils/auth.js');
 Component({
     /**
      * 组件的属性列表
@@ -12,13 +13,16 @@ Component({
      * 组件的初始数据
      */
     data: {
-        color: '#ee0a24',
+        isDone: false,
         value: [100, 200],
         valueRuler: [30, 84, 138, 192, 246, 300],
         status: 1,
         budget_min: 100,
         budget_max: 200,
         positionList: [],
+        contact_sex: 1,
+        contact_name: '',
+        sms_code: '',
         housetypeList: [{
                 name: '1居',
                 value: 1,
@@ -73,6 +77,24 @@ Component({
 
     attached() {
         this.loadData()
+
+        var _this = this
+        app.ensureConfigs((myconfigs) => {
+            _this.setData({
+                primaryColor: myconfigs.color.primary,
+                primaryBtnColor: myconfigs.color.primary_btn
+            })
+        })
+
+        var user = app.globalData.userInfo
+        var contact_mobile_lock = false
+        if (user.mobile && user.mobile.length == 11) {
+            contact_mobile_lock = true
+        }
+        this.setData({
+            contact_mobile: user.mobile,
+            contact_mobile_lock: contact_mobile_lock
+        })
     },
 
     /**
@@ -244,7 +266,7 @@ Component({
             var ps = this.data.purposeList
             var p = ps[i]
             var purpose = []
-    
+
             p.selected = !p.selected
             ps[i] = p
             this.data.purposeList.forEach(function (item, i) {
@@ -253,12 +275,155 @@ Component({
                     purpose.push(item.name)
                 }
             })
-    
-    
+
+
             this.setData({
                 purposeList: ps,
                 purpose: purpose.join(',')
             })
+        },
+        sexToggle: function (e) {
+            this.setData({
+                contact_sex: this.data.contact_sex == 1 ? 0 : 1,
+            })
+        },
+        inputChange: function (e) {
+            var key = e.target.dataset.name
+            var value = e.detail
+            var d = {}
+            d[key] = value
+            this.setData(d)
+        },
+        changeMobile: function (e) {
+            this.setData({
+                contact_mobile: '',
+                contact_mobile_lock: false,
+            })
+        },
+        submitHandle: function (e) {
+            var _this = this
+            _this.validate(function (data) {
+                // 如果需要提交验证码
+                // 应该改为，判断点击了修改按钮之后
+                var _token = app.globalData.token
+                if (!_this.data.contact_mobile_lock) {
+                    _this.smsLoginHandle(() => {
+                        _this.postData(data)
+                    })
+                    return
+                }
+                _this.postData(data)
+                return
+            })
+
+        },
+        smsLoginHandle(cb) {
+            // 通过短信验证码登陆账号
+            var phone = this.data.contact_mobile
+            var code = this.data.sms_code
+            if (!(/^1[3456789]\d{9}$/.test(phone))) {
+                wx.showModal({
+                    title: '手机号格式错误',
+                    icon: 'none'
+                })
+                return false
+            }
+            if (code.length != 4) {
+                wx.showModal({
+                    title: '验证码输入错误',
+                    icon: 'none'
+                })
+                return false
+            }
+            app.request({
+                url: '/api/v1/sms/auth',
+                method: 'POST',
+                data: {
+                    mobile: phone,
+                    code: code
+                },
+                success: function (res) {
+                    var data = res.data
+                    if (data.status == 0) {
+                        // 保存下服务器返回的token
+                        var token = data.data.token
+                        var user = data.data.user
+                        auth.setUserInfo(token, user)
+                        return cb()
+                    }
+                }
+            })
+        },
+        postData: function (data) {
+            var _this = this
+            // 将表单数据处理成线索表所需要的数据格式 
+
+
+            app.request({
+                url: '/api/v1/needs/',
+                data: data,
+                method: 'POST',
+                success: function (resp) {
+                    if (resp.data.status != 0) {
+                        wx.showToast({
+                            title: '服务器出现错误，请稍后再试',
+                            icon: 'none',
+                        })
+                        return false
+                    }
+
+                    // 设置一些重要标志位 
+                    _this.setData({
+                        isDone: true,
+                        sms_code: null,
+                        contact_mobile_lock: true,
+                    })
+                    wx.showToast({
+                        title: '提交成功，我们的专业顾问将尽快与您联系',
+                    })
+                    // setTimeout(() => {
+                    //   wx.navigateBack({
+                    //     delta: 1,
+                    //   })
+                    // },1500)
+                }
+            })
+        },
+        validate: function (cb) {
+            if (!this.data.contact_name) {
+                wx.showToast({
+                    title: '请填写联系人姓名',
+                    icon: 'none'
+                })
+                return
+            }
+            if (this.data.contact_mobile_lock == false && !this.data.sms_code) {
+                wx.showToast({
+                    title: '请填写短信验证码',
+                    icon: 'none'
+                })
+                return
+            }
+            var data = {
+                cat: this.data.cat,
+                budget_min: this.data.budget_min,
+                budget_max: this.data.budget_max,
+                position: this.data.position,
+                housetype: this.data.housetype,
+                area: this.data.area,
+                points: this.data.purpose,
+                name: this.data.contact_name,
+                mobile: this.data.contact_mobile,
+                sex: this.data.contact_sex,
+            }
+            var budget_max = this.data.budget_max
+            if (budget_max == 300) {
+                delete data.budget_max
+            }
+            return cb(data)
+        },
+        reWrite: function(e){
+            this.setData({isDone: false,status: 1})
         },
     }
 })
