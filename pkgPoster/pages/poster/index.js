9dae30e7e7a6c4d4c644ec4a4a209b6da61288e8
yohn <yohn@udeve.cn>
2022-07-21 17:46:52 +0800

    update

--- pkgPoster/pages/poster/index.js
+++ pkgPoster/pages/poster/index.js
@@ -3,31 +3,6 @@ const app = getApp()
 import Poster from '../../utils/poster/poster';
 var auth = require('../../../utils/auth.js');
 
-const xxx =     {
-    type: 'text',
-    text: "足不出户在线看新房",
-    css: {
-        width: '300rpx',
-        height: '40rpx',
-        fontSize: "30rpx",
-        top: "185rpx",
-        left: "30rpx",
-        color: "#FFFFFF"
-    }
-}
-
-
-var  yyy = {
-    type: 'image',
-    url: '',
-    css: {
-      width: '690rpx',
-      height: '465rpx',
-      top: '290rpx',
-      left: '30rpx',
-    },
-  },
-
 Page({
 
     /**
@@ -163,7 +138,6 @@ Page({
         // 二维码携带的额外参数
         var qrdata = {
             id: this.data.postId, 
-            bindBrokerId: uinfo.id, // 客户注册后，将绑定
             scene_key: 'poster',
             source_uid: uinfo.id,  
         }
@@ -187,10 +161,201 @@ Page({
 
     },
 
+    genPoster: function () {
+        // 生成海报数据结构
+        var post = this.data.post
 
-    genTags: function(){
-        var tags = this.data.post.tags.filter((t) => {})
+        var qrUrl = this.data.qrUrl
+        var post = this.data.post
+        var tpl = this.data.tpls[this.data.tplIndex]
+        var averagePrice = this.data.post.average_price.split('~')[0]
+        var bgImage = tpl.bg
+        var addressIcon = '/assets/icons/haibao-address.png'
 
+        var userInfo = app.globalData.userInfo
+        var fontColor = tpl.font_color || '#ffffff'
+
+        var palette = {
+            background: bgImage,
+            width: '750rpx',
+            height: '1550rpx',
+            views: [
+            {
+                type: 'text',
+                text: "楼盘海报",
+                css: {
+                    width: '300rpx',
+                    height: '80rpx',
+                    fontSize: "60rpx",
+                    top: "97rpx",
+                    left: "30rpx",
+                    color: "#FFFFFF",
+                    fontWeight: "bold"
+                }
+            },
+            {
+                type: 'text',
+                text: "足不出户在线看新房",
+                css: {
+                    width: '300rpx',
+                    height: '40rpx',
+                    fontSize: "30rpx",
+                    top: "185rpx",
+                    left: "30rpx",
+                    color: "#FFFFFF"
+                }
+            },
+            {
+              type: 'image',
+              url: post.cover,
+              css: {
+                width: '690rpx',
+                height: '465rpx',
+                top: '290rpx',
+                left: '30rpx',
+              },
+            },
+            {
+                type: 'rect',
+                css: {
+                  width: '690rpx',
+                  height: '361rpx',
+                  color: '#FFFFFF',
+                  top: '755rpx',
+                  left: '30rpx',
+                },
+            },
+            {
+                type: 'text',
+                text: post.title,
+                css: {
+                    width: '580rpx',
+                    height: '53rpx',
+                    fontSize: "40rpx",
+                    top: "788rpx",
+                    left: "68rpx",
+                    fontWeight: "bold",
+                    maxLines: 1
+                }
+            },
+            {
+                type: 'image',
+                url: addressIcon,
+                css: {
+                  width: '20rpx',
+                  height: '24rpx',
+                  top: '937rpx',
+                  left: '70rpx',
+                },
+            },
+            {
+                type: 'text',
+                text: '地址：' + post.address,
+                css: {
+                    width: '550rpx',
+                    height: '34rpx',
+                    fontSize: "26rpx",
+                    top: "932rpx",
+                    left: "110rpx",
+                    maxLines: 1
+                }
+            },
+            {
+                type: 'text',
+                text: '建面',
+                css: {
+                    width: '68rpx',
+                    height: '32rpx',
+                    fontSize: "24rpx",
+                    color: "#AFAFAF",
+                    top: "996rpx",
+                    left: "68rpx",
+                }
+            },
+            {
+                type: 'text',
+                text: post.area_info.text + post.area_info.px,
+                css: {
+                    width: '220rpx',
+                    height: '40rpx',
+                    fontSize: "30rpx",
+                    fontWeight: "bold",
+                    top: "1037rpx",
+                    left: "68rpx",
+                }
+            },
+            {
+                type: 'text',
+                text: '均价',
+                css: {
+                    width: '68rpx',
+                    height: '32rpx',
+                    fontSize: "24rpx",
+                    color: "#AFAFAF",
+                    top: "996rpx",
+                    left: "297rpx",
+                }
+            },
+            {
+                type: 'text',
+                text: averagePrice + post.average_price_info.px,
+                css: {
+                    width: '220rpx',
+                    height: '40rpx',
+                    fontSize: "30rpx",
+                    fontWeight: "bold",
+                    color: "#E46C69",
+                    top: "1037rpx",
+                    left: "297rpx",
+                }
+            },
+            {
+                type: 'text',
+                text: '装修',
+                css: {
+                    width: '68rpx',
+                    height: '32rpx',
+                    fontSize: "24rpx",
+                    color: "#AFAFAF",
+                    top: "996rpx",
+                    left: "556rpx",
+                }
+            },
+            {
+                type: 'text',
+                text: post.fitment,
+                css: {
+                    width: '220rpx',
+                    height: '40rpx',
+                    fontSize: "30rpx",
+                    fontWeight: "bold",
+                    top: "1037rpx",
+                    left: "556rpx",
+                }
+            },
+            {
+                type: 'rect',
+                css: {
+                  width: '143rpx',
+                  height: '143rpx',
+                  color: '#FFFFFF',
+                  borderRadius: '72rpx',
+                  top: '1176rpx',
+                  left: '552rpx',
+                },
+            },
+            {
+                type: 'image',
+                url: qrUrl,
+                css: {
+                  width: '127rpx',
+                  height: '127rpx',
+                  top: '1184rpx',
+                  left: '560rpx',
+                },
+            },
+            ],
+        }
         var haibaoTags =[
             {
                 type: 'text',
@@ -231,117 +396,80 @@ Page({
                 }
             })
         }
-        return tags 
-    },
+        haibaoTags.forEach((item) => {
+            palette.views.push(item)
+        })
 
-    genPoster: function () {
-        // 生成海报数据结构
-        var post = this.data.post
-
-        var qrUrl = this.data.qrUrl
-        var post = this.data.post
-        var tpl = this.data.tpls[this.data.tplIndex]
-        var averagePrice = this.data.post.average_price.split('~')[0]
-        var bgImage = tpl.bg
-        var addressIcon = '/assets/icons/haibao-address.png'
-
-        var userInfo = app.globalData.userInfo
-        var fontColor = tpl.font_color || '#ffffff'
-        yyy.url = post.cover  
-        xxx.text = 'hel'
-        var tagx = this.genTgas() 
-        var brokerQr = this.genBrokerQr()
-
-        var palette = {
-            background: bgImage,
-            width: '750rpx',
-            height: '1550rpx',
-            views: [
-            xxx,
-            xxy,
-            yyy, 
-            tagx, 
-            brokerQr, 
-     
-            ],
+        // 如果是置业顾问，显示头像和姓名
+        if(userInfo && userInfo.is_broker){
+           palette.views.push(
+            {
+                type: 'image',
+                url: userInfo.avatar,
+                css: {
+                  width: '47rpx',
+                  height: '47rpx',
+                  borderRadius: '23rpx',
+                  top: '1187rpx',
+                  left: '55rpx',
+                },
+            },
+            {
+                type: 'text',
+                text: userInfo.name,
+                css: {
+                    width: '300rpx',
+                    height: '48rpx',
+                    fontSize: "36rpx",
+                    top: "1187rpx",
+                    left: "122rpx",
+                    color: "#FFFFFF",
+                }
+            },
+            {
+                type: 'text',
+                text: '专业、优质服务',
+                css: {
+                    width: '394rpx',
+                    height: '32rpx',
+                    fontSize: "24rpx",
+                    top: "1245rpx",
+                    left: "57rpx",
+                    color: "#FFFFFF",
+                }
+            },
+            {
+                type: 'text',
+                text: '长按识别小程序码查看详情',
+                css: {
+                    width: '394rpx',
+                    height: '32rpx',
+                    fontSize: "24rpx",
+                    top: "1282rpx",
+                    left: "57rpx",
+                    color: "#FFFFFF",
+                }
+            },
+           )
+        }else{
+            palette.views.push(
+                {
+                    type: 'text',
+                    text: '长按识别小程序码查看详情',
+                    css: {
+                        width: '394rpx',
+                        height: '32rpx',
+                        fontSize: "24rpx",
+                        top: "1245rpx",
+                        left: "57rpx",
+                        color: "#FFFFFF",
+                    }
+                },
+               )        
         }
-    },
-
-
- 
 
         this.setData({ palette: palette})
     },
-
-    genBrokerQr(){
-               // 如果是置业顾问，显示头像和姓名
-               if(userInfo && userInfo.is_broker){
-                palette.views.push(
-                 {
-                     type: 'image',
-                     url: userInfo.avatar,
-                     css: {
-                       width: '47rpx',
-                       height: '47rpx',
-                       borderRadius: '23rpx',
-                       top: '1187rpx',
-                       left: '55rpx',
-                     },
-                 },
-                 {
-                     type: 'text',
-                     text: userInfo.name,
-                     css: {
-                         width: '300rpx',
-                         height: '48rpx',
-                         fontSize: "36rpx",
-                         top: "1187rpx",
-                         left: "122rpx",
-                         color: "#FFFFFF",
-                     }
-                 },
-                 {
-                     type: 'text',
-                     text: '专业、优质服务',
-                     css: {
-                         width: '394rpx',
-                         height: '32rpx',
-                         fontSize: "24rpx",
-                         top: "1245rpx",
-                         left: "57rpx",
-                         color: "#FFFFFF",
-                     }
-                 },
-                 {
-                     type: 'text',
-                     text: '长按识别小程序码查看详情',
-                     css: {
-                         width: '394rpx',
-                         height: '32rpx',
-                         fontSize: "24rpx",
-                         top: "1282rpx",
-                         left: "57rpx",
-                         color: "#FFFFFF",
-                     }
-                 },
-                )
-             }else{
-                 palette.views.push(
-                     {
-                         type: 'text',
-                         text: '长按识别小程序码查看详情',
-                         css: {
-                             width: '394rpx',
-                             height: '32rpx',
-                             fontSize: "24rpx",
-                             top: "1245rpx",
-                             left: "57rpx",
-                             color: "#FFFFFF",
-                         }
-                     },
-                    )        
-             }
-    }
 
     onImgOK(e) {
         var imagePath = e.detail.path;
