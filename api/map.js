09874fc51a3a6c020dc60a29e45be50df905e7c3
yohn <yohn@udeve.cn>
2023-06-14 15:27:20 +0800

    增加了geocoder接口

--- api/map.js
+++ api/map.js
@@ -4,5 +4,10 @@ let request = require('../utils/request.js');
 // 拉取地图标记点  1
 export function  getMapMarkerList(query){
     return request.post("/api/v1/map_markers", query);
+}
+
+
+export function geocoder(latitude, longitude){
+    return request.post("/api/v1/geocoder", {latitude:latitude, longitude:longitude});
 
 }
