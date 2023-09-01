71c7401b1c90cbcefddf1c758be749cf38354767
yohn <yohn@udeve.cn>
2023-09-01 15:17:37 +0800

    注意，java后端只能接收下划线风格的参数

--- api/session.js
+++ api/session.js
@@ -4,5 +4,5 @@ let request = require('../utils/request.js');
 // 微信手机号码授权登录
 // 必须传递对应的3个参数  1
 export function wechatLogin( code, encryptedData, iv){
-    return request.post("/api/v6/sessions", {code: code, iv: iv, encryptedData: encryptedData})
+    return request.post("/api/v6/sessions", {code: code, iv: iv, encrypted_data: encryptedData})
 }
