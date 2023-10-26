20381909aa64b41263270e5eb53101863aed8922
udeveXu <1500730439@qq.com>
2023-10-26 13:26:05 +0800

    助力活动接口更新

--- api/zhuli.js
+++ api/zhuli.js
@@ -3,28 +3,28 @@ let request = require('../utils/request.js');
 
 // 好友点击助力  1
 export function  createZhuliHaoyou(data){
-    return request.post("/api/v1/zhuli_haoyou", data);
+    return request.post("/api/v6/zhuli_haoyou", data);
 
 }
 
 // 查询好友助力情况  1
 export function  getZhuliHaoyouList(query){
-    return request.get("/api/v1/zhuli_haoyou", query);
+    return request.get("/api/v6/zhuli_haoyou", query);
 }
 
 // 查询助力活动的详情   1
 export function  getZhuliDetail(zhuliId){
-    return request.get("http://localhost:8080/api/v6/zhuli/" + zhuliId);
+    return request.get("/api/v6/zhuli/" + zhuliId);
 }
 
 
 // 助力成功，领取奖励  1
 export function  createZhuliSuccess(data){
-    return request.post("/api/v1/zhuli", data);
+    return request.post("/api/v6/zhuli", data);
 }
 
 // 发起我的助力活动  完成
 export function joinZhuli(hid){
-    return request.post("/api/v1/zhuli/join/", {id: hid});
+    return request.post("/api/v6/zhuli/join/", {id: hid});
 
 }
